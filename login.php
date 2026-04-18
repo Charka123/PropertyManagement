7696d141f7002109fb2db3bf1b5e3f59a9bfc460
Charka123 <donghanzhe05@gmail.com>
2026-04-18 15:25:57 -0400

    Basic login page, not working yet

--- login.php
+++ login.php
@@ -6,6 +6,30 @@
     <title>Document</title>
 </head>
 <body>
-    <p>Something</p>
+    <h1>Property Management</h1>
+    <p>Sign in to your account</p>
+    <form action="login.php" method="POST">
+        <h3>Email address</h3>
+        <input type="email" name="email" placeholder="Email" required>
+        <h3>Password</h3>
+        <input type="password" name="password" placeholder="Password" required>
+        <h3>Login as</h3>
+        <label>
+            <input type="radio" name="user_role" value="tenant" checked>
+            Tenant (submit and track maintenance requests)
+        </label>
+        <br>
+        <label>
+            <input type="radio" name="user_role" value="owner">
+            Property Owner (manage property and maintenance)
+        </label>
+        <br>
+        <label>
+            <input type="radio" name="user_role" value="worker">
+            Maintenance Company (browse and apply for jobs)
+        </label>
+        <br>
+        <button type="submit">Sign in</button>
+</form>
 </body>
 </html>
